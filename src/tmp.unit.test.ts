8eb831297026bc80c8bb45227975356613c97c15
Kevin Eger <egerkevinjames@gmail.com>
2024-12-03 10:56:03 -0800

    ci: configure unit test automation

---
+++ src/tmp.unit.test.ts
@@ -0,0 +1,8 @@
+import { equal } from "assert";
+import { describe, it } from "mocha";
+
+describe("Temporary unit tests", () => {
+    it("should be able to execute", () => {
+        equal(true, true);
+    });
+});
