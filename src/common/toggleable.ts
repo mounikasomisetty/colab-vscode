96682d97af09d2c762d8903a7a7b91367ee2d6d9
Kevin Eger <egerkevinjames@gmail.com>
2025-11-21 01:34:34 +0000

    wip: simplify eventing/toggling/running async tasks from sync listeners

--- src/common/toggleable.ts
+++ src/common/toggleable.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { log } from "./logging";
+import { LatestCancelable } from "./async";
 
 /**
  * An entity which can be turned "on" and "off".
@@ -23,12 +23,6 @@ export interface Toggleable {
 
 type ToggleDirection = Lowercase<keyof Toggleable>;
 
-class ToggleAbortedError extends Error {
-  constructor(from: ToggleDirection, to: ToggleDirection) {
-    super(`Toggling from ${from} superseded by toggle to ${to}`);
-  }
-}
-
 /**
  * Manages toggling on and off asynchronously.
  *
@@ -36,73 +30,27 @@ class ToggleAbortedError extends Error {
  * when toggling.
  */
 export abstract class AsyncToggle implements Toggleable {
-  private inFlight?: { ctrl: AbortController; to: ToggleDirection };
+  private runner = new LatestCancelable<[ToggleDirection]>(
+    "AsyncToggle",
+    async (to, signal) => {
+      switch (to) {
+        case "on":
+          await this.turnOn(signal);
+          break;
+        case "off":
+          await this.turnOff(signal);
+          break;
+      }
+    },
+  );
+
+  on(): void {
+    void this.runner.run("on");
+  }
+  off(): void {
+    void this.runner.run("off");
+  }
 
   protected abstract turnOn(signal: AbortSignal): Promise<void>;
   protected abstract turnOff(signal: AbortSignal): Promise<void>;
-
-  on(): void {
-    void this.toggle("on");
-  }
-
-  off(): void {
-    void this.toggle("off");
-  }
-
-  protected async toggle(to: ToggleDirection): Promise<void> {
-    if (this.inFlight) {
-      // Already toggling in that direction.
-      if (this.inFlight.to === to) {
-        return;
-      }
-      this.inFlight.ctrl.abort(new ToggleAbortedError(this.inFlight.to, to));
-    }
-
-    const ctrl = new AbortController();
-    this.inFlight = { ctrl, to };
-
-    try {
-      await (to === "on"
-        ? this.turnOn(ctrl.signal)
-        : this.turnOff(ctrl.signal));
-
-      // If this is no longer the active operation, stop.
-      if (!this.hasActiveControl(ctrl)) {
-        log.trace(`Completed toggle to ${to} but a new one superseded it.`);
-        return;
-      }
-
-      log.trace(`Async toggle to ${to} completed successfully.`);
-    } catch (err: unknown) {
-      // Toggling superseded, this no longer has active control.
-      if (ctrl.signal.aborted) {
-        log.trace(`Async toggle aborted.`, err);
-        return;
-      }
-
-      // A non-aborted failure.
-      log.error(`Async toggle to ${to} failed unexpectedly.`, err);
-      throw err;
-    } finally {
-      // Only clear if this is still the active operation.
-      if (this.hasActiveControl(ctrl)) {
-        this.inFlight = undefined;
-      }
-    }
-  }
-
-  /**
-   * Checks if the given controller matches the active in-flight operation.
-   *
-   * Within {@link AsyncToggle.toggle | toggle}, the `await` call yields
-   * control to the event loop. In that gap, `this.inFlight` can change
-   * (specifically, it can be set to undefined by the finally block of a
-   * superseding call).
-   *
-   * While the check is trivial, it's lifted to make the seemingly redundant
-   * checks more obvious, with justification.
-   */
-  private hasActiveControl(ctrl: AbortController): boolean {
-    return this.inFlight?.ctrl === ctrl;
-  }
 }
